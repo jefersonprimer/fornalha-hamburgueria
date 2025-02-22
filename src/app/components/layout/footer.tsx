98e5c487ccd92e0176b63e3d16437b0d582f008a
jefersonpriimer <jefersonpriimer@gmail.com>
2025-02-22 10:52:28 +0000

    feat: footer

---
+++ src/app/components/layout/footer.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto text-center">
+        <p>&copy; {new Date().getFullYear()} Fornalha Hamburgueria. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
+import Footer from "./components/layout/footer";
 import Header from "./components/layout/header"
 
 const Home = () => {
   return (
     <>
       <Header/>
+      <Footer />
     </>
   )
 }
